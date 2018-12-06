529559a4fdbba99893c21611f80d0be0e984776a
Trazard <34913527+Trazard@users.noreply.github.com>
2018-12-06 22:38:15 +0100

    average.py done

---
+++ airbus/average.py
@@ -0,0 +1,20 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from utils import get_array_from_img
+from utils import aff_mat_2D
+
+m = get_array_from_img("Nuit_Info_2018\\airbus\\image.jpeg",100,100)
+print(m)
+
+def get_dom_color_img(mat):
+	somR = 0
+	somG = 0
+	somB = 0
+	nb_pixel = len(mat) * len(mat[0])
+	for ligne in mat:
+		for (r,g,b) in ligne:
+			somR += r
+			somG += g
+			somB += b
+	return (int(somR/nb_pixel), int(somG/nb_pixel), int(somB/nb_pixel))
