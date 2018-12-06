deba65146a54607f983de787e3c47494b209d6d1
Digo <romero.diego31@gmail.com>
2018-12-06 21:42:23 +0100

    eee

--- airbus/normalize_img.py
+++ airbus/normalize_img.py
@@ -1,6 +1,10 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-imgpil = Image.open("Nuit_Info_2018\\airbus\\image.jpeg")  
-img = np.asarray(imgpil)
+
+
+
+
 print(img)
+
+aff_mat_2D(img)
