be1754ddcb560c0ed84e7ededb492a4a16819c0c
Trazard <34913527+Trazard@users.noreply.github.com>
2018-12-06 22:03:55 +0100

    resized img ok

--- airbus/normalize_img.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-
-
-def normalize_img(path, l, h):
-	
--- airbus/utils.py
+++ airbus/utils.py
@@ -1,10 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
 
-
-def get_array_from_img(path):
-	return np.asarray(plt.imread(path))
+def get_array_from_img(path, sizeX, sizeY):
+	img = Image.open(path)
+	img = img.resize((sizeX, sizeY), Image.ANTIALIAS)
+	return np.asarray(img)
 
 def aff_mat_2D(matrice):
 	plt.imshow(matrice)
 	plt.show()
+
+
+"""
+Exemple : 
+import utils
+img = utils.get_array_from_img("Nuit_Info_2018\\airbus\\image.jpeg", 100, 100)
+utils.aff_mat_2D(img)
+"""
