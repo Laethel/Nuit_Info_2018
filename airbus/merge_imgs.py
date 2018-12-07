e19fdc40ea7f46582eb6e50a9c7dc2008840c534
SebPou <sebastien.poussard@protonmail.ch>
2018-12-07 01:11:33 +0100

    merge_imgs prend une matrice

--- airbus/merge_imgs.py
+++ airbus/merge_imgs.py
@@ -2,28 +2,27 @@ from PIL import Image
 import os 
  
  
-# concatene des image de la liste d'adresse d'images "liste" 
-# en mettant "width_f" nombre d'images en largeur 
-# et "height_f" nombre d'image en hauteur 
+# concatene des image de la matrice de chemin d'image "matrix"
+# sauvegarde le résultat dans le path donné "save_path"
  
-def merge_imgs(liste, width_f, height_f): 
-    img = Image.open(liste[0]) 
+def merge_imgs(matrix,save_path): 
+    img = Image.open(matrix[0][0]) 
     width_image, height_image = img.size 
-    res = Image.new("RGB",(width_f * width_image, height_f * height_image)) 
+    res = Image.new("RGB",(len(matrix) * width_image, len(matrix[0]) * height_image)) 
     curseur_x = 0 
     curseur_y = 0 
-    for image in liste: 
-        img = Image.open(image) 
-        print(curseur_x,curseur_y) 
-        if (curseur_x >= width_image * width_f): 
-            curseur_x = 0 
-            curseur_y += height_image 
-        res.paste(img, box=(curseur_x, curseur_y)) 
-        curseur_x += width_image 
-    #res.save(r"C:\Users\VM\Desktop\py\Nuit_Info_2018\airbus\toto.jpg") 
+    for j in matrix:
+         for i in j:
+            img = Image.open(i) 
+            if (curseur_x >= width_image * len(matrix) ): 
+                curseur_x = 0 
+                curseur_y += height_image 
+            res.paste(img, box=(curseur_x, curseur_y)) 
+            curseur_x += width_image 
+    res.save(save_path) 
          
     return res 
  
-if __name__ == "__main__": 
-    liste_images = [r"C:\Users\VM\Desktop\py\Nuit_Info_2018\airbus\test.jpg", r"C:\Users\VM\Desktop\py\Nuit_Info_2018\airbus\test2.jpg", r"C:\Users\VM\Desktop\py\Nuit_Info_2018\airbus\test2.jpg"] 
-    merge_imgs(liste_images, 1, 3).show()
+# if __name__ == "__main__": 
+#     liste_images = [[r"C:\Users\VM\Desktop\py\Nuit_Info_2018\airbus\image.jpeg", r"C:\Users\VM\Desktop\py\Nuit_Info_2018\airbus\image.jpeg"], [r"C:\Users\VM\Desktop\py\Nuit_Info_2018\airbus\image.jpeg", r"C:\Users\VM\Desktop\py\Nuit_Info_2018\airbus\image.jpeg"] ]
+#     merge_imgs(liste_images,r"C:\Users\VM\Desktop\py\Nuit_Info_2018\airbus\toto.jpg").show()
